e0f69fd5ed8266587630dc29d85741cfb6c908a3
Huyemt <778600096@qq.com>
2022-07-03 00:41:26 +0800

    update

--- src/main/java/korara/Korara.java
+++ src/main/java/korara/Korara.java
@@ -1,11 +1,11 @@
 package korara;
 
 import javafx.application.Application;
-import korara.javafx.Main;
+import korara.ui.Main;
 
 public class Korara {
+
     public static void main(String[] args) {
         Application.launch(Main.class);
-        //my test
     }
 }
