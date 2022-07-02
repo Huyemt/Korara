1127094aa43187370f73dc4b9da7f621c058216d
Huyemt <778600096@qq.com>
2022-07-03 01:14:01 +0800

    updated Korara Main UI

--- src/main/java/korara/ui/Main.java
+++ src/main/java/korara/ui/Main.java
@@ -1,11 +1,17 @@
 package korara.ui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
+        Scene scene = new Scene(new Pane(), 500, 500);
+        stage.setScene(scene);
+
+
         stage.setTitle("Korara");
         stage.show();
     }
